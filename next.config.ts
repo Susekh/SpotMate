914f0405bb5dea67320a46aa51de3e7c923ea4b5
Subh <subhranshukhilar@outlook.com>
2025-11-10 01:28:00 +0530

    disabled lightning css

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    serverExternalPackages : ['pino', 'pino-pretty'],
-    images : {
-        remotePatterns :  [{hostname : "res.cloudinary.com"}],
-    },
+  serverExternalPackages: ["pino", "pino-pretty"],
+  experimental: {
+    optimizeCss: false, // Disable Lightning CSS
+  },
+  images: {
+    remotePatterns: [{ hostname: "res.cloudinary.com" }],
+  },
 };
 
 export default nextConfig;
